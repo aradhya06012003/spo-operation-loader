0cc6e12e82939fb421b8b866bcb048d8aaea4199
Aradhya <aradhyahindalekar@gmail.com>
2026-06-15 00:10:33 +0530

    Created spring app, model tables

---
+++ src/main/java/com/spo/core_app/CoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.spo.core_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spo/core_app/models/GlobalRecord.java
@@ -0,0 +1,18 @@
+package com.spo.core_app.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "global_records")
+public class GlobalRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID sysId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+}
